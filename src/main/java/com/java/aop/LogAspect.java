ab3631197cf0d9edf0204a02c4ed588518ef6fea
Vulpes94 <kjunfi94@gmail.com>
2021-12-29 15:43:05 +0900

    aop annotation 적용.

--- src/main/java/com/java/aop/LogAspect.java
+++ src/main/java/com/java/aop/LogAspect.java
@@ -3,21 +3,28 @@ package com.java.aop;
 import java.util.logging.Logger;
 
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
 
+@Component
+@Aspect
 public class LogAspect { // 공통클래스
-	public static Logger logger = Logger.getLogger(LogAspect.class.getName());
-	public static final String LogMsg = "LogMsg--------";
+  public static Logger logger = Logger.getLogger(LogAspect.class.getName());
+  public static final String LogMsg = "LogMsg--------";
 
-	public Object advice(ProceedingJoinPoint joinPoint) { // 공통함수
-		Object obj = null;
+  @Around(value = "within(com.java..*)")
+  public Object advice(ProceedingJoinPoint joinPoint) { // 공통함수
+    Object obj = null;
 
-		try {
-			// logger.info(LogMsg + joinPoint.getTarget().getClass().getName() + "\t" + joinPoint.getSignature().getName());
-			obj = joinPoint.proceed();
-		} catch (Throwable e) {
-			e.printStackTrace();
-		}
+    try {
+      // logger.info(LogMsg + joinPoint.getTarget().getClass().getName() + "\t" +
+      // joinPoint.getSignature().getName());
+      obj = joinPoint.proceed();
+    } catch (Throwable e) {
+      e.printStackTrace();
+    }
 
-		return obj;
-	}
+    return obj;
+  }
 }
